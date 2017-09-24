c11f42fc5601c081cfdaae4e988f6ad90b1523e4
a1rohner <a1rohner@hsr.ch>
2017-09-24 21:29:12 +0200

    openFoam vm v0.1

--- UbuntuOpenFOAM-VM/README.md
+++ UbuntuOpenFOAM-VM/README.md
@@ -1,5 +1,10 @@
 # OpenFOAM on Linux Ubuntu
 
-<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2Fapache2-on-ubuntu-vm%2Fazuredeploy.json" target="_blank"><img src="http://azuredeploy.net/deploybutton.png"/></a>
-<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fa1rohner%2FAzure-scripts%2Fmaster%2FUbuntuOpenFOAM-VM%2Fazuredeploy.json" target="_blank"><img src="http://armviz.io/visualizebutton.png"/></a>
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fa1rohner%2FAzure-scripts%2Fmaster%2FUbuntuOpenFOAM-VM%2Fazuredeploy.json" target="_blank">
+	<img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fa1rohner%2FAzure-scripts%2Fmaster%2FUbuntuOpenFOAM-VM%2Fazuredeploy.json" target="_blank">
+	<img src="http://armviz.io/visualizebutton.png"/>
+</a>
+
 This template uses the Azure Linux CustomScript extension to deploy a VM with OpenFOAM pre-installed. The template creates an Ubuntu VM, installs OpenFOAM 5 with paraview.
--- UbuntuOpenFOAM-VM/install_openfoam.sh
+++ UbuntuOpenFOAM-VM/install_openfoam.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-apt-get -y update
 
-# install Apache2
-apt-get -y install apache2 
+# install openFOAM
+add-apt-repository http://dl.openfoam.org/ubuntu 
+sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
+apt-get update
+apt-get -y install openfoam5
 
 # write some HTML
-echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+echo 'source /opt/openfoam5/etc/bashrc' >> ~/.bashrc
 
 # restart Apache
-apachectl restart
+apt-get install xrdp
+
+# Step 2 – Install XFCE4 ( Unity doesn't seem to support xRDP in Ubuntu 14.04; although, in Ubuntu 12.04 it was supported ). 
+# That's why we install Xfce4.
+
+apt-get install xfce4
+
+# Step 3 – Configure xRDP
+# In this step, we modify two files to make sure xRDP uses Xfce4. 
+# First we need to create, or edit, our .xsession file in our home directory. 
+# We can either use nano or simply redirect an echo statement (easier):
+
+echo 'xfce4-session' >> ~/.xsession
+
+# The second file we need to edit is the startup file for xRDP, so it will start Xfce4.
+
+echo 'startxfce4' >> /etc/xrdp/startwm.sh
+
+# Step 4 – Restart xRDP
+# To make all these changes effective, restart xRDP as such:
+
+service xrdp restart
